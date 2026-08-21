b1067a3e1587e6721ff8524949892421cbffabb6
Andres Badel <andres.badel@zentstudio.net>
2026-08-21 10:38:52 -0500

    link to <P>

--- app/[app]/[doc]/page.tsx
+++ app/[app]/[doc]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
 import { getDoc } from "@/lib/docs";
 
 export default async function DocPage({
@@ -14,7 +15,7 @@ export default async function DocPage({
 
   return (
     <main style={{ maxWidth: 720, margin: "0 auto", padding: "3rem 1.5rem", lineHeight: 1.6 }}>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSlug]}>{content}</ReactMarkdown>
     </main>
   );
 }
